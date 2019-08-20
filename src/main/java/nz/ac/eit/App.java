18482238caa081494018824d5a18f1180e96d665
Mark Lynskey <mlynskey2@gmail.com>
2019-08-20 14:57:13 +1200

    Issue #1 Asked for a number to translate and display this number.

--- src/main/java/nz/ac/eit/App.java
+++ src/main/java/nz/ac/eit/App.java
@@ -1,5 +1,7 @@
 package nz.ac.eit;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World changed!" );
+        System.out.println( "What is the number to translate?" );
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        int num = Integer.parseInt(input);
+        System.out.println("The number to translate is " + num);
     }
 }
